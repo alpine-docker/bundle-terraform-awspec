c037396e1b4b8af86eb26da557f73d655ec6e0bc
Bill Wang <bill.wang@team.telstra.com>
2018-06-24 18:27:12 +1000

    Add travis build pipeline

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
+# Prerequisite
+# Make sure you set secret enviroment variables: DOCKER_USERNAME and  DOCKER_PASSWORD in Travis CI
+
 image="alpine/bundle-terraform-awspec"
+tag="1.0"
 
 docker build --no-cache -t ${image} .
-docker tag ${image} ${image}:1.0
+docker tag ${image} ${image}:${tag}
 
   if [[ "$TRAVIS_BRANCH" == "master" ]]; then
-    docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+    docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
     docker push ${image}
-    docker push ${image}:1.0
+    docker push ${image}:${tag}
   fi
